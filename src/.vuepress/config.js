806006ba45ec60b0723fc343b09e7363f514b538
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-05-09 13:11:57 +0200

    feat: 🎸 init files

---
+++ src/.vuepress/config.js
@@ -0,0 +1,87 @@
+const { description } = require('../../package')
+
+module.exports = {
+  theme: 'cosmos',
+  locales: {
+    '/': {
+      lang: 'en-US',
+    },
+  },
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Elys Protocol Documentation',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'elys-network/elys-docs',
+    editLinks: true,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    // docs 1.0.168: custom true hides subpages searchbar
+    // docs 1.0.168: custom true hides hub, ibc, core sidebar footer logos
+    custom: true,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    topbar: {
+      banner: false,
+    },
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    'tabs',
+    [
+      '@maginapp/vuepress-plugin-katex',
+      {
+        delimiters: 'dollars',
+      },
+    ],
+  ]
+}
