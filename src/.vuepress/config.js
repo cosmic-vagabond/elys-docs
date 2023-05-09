cc6bb66af2f057730dbf363cc5a511f23bfa8084
Ollie Ottersync <105137269+ollieottersync@users.noreply.github.com>
2023-05-09 07:43:01 -0700

    change protocol to network

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Elys Protocol Documentation',
+  title: 'Elys Network Documentation',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
